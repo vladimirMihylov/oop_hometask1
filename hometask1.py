799d8e0b5ed5cd9570f0f8620139106271e35485
Vladimir Mihylov <vladimir.mhylv@yandex.ru>
2023-11-07 13:14:50 +0300

    rate_lecturer feature added

--- hometask1.py
+++ hometask1.py
@@ -6,6 +6,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.lecturer_grades and 1 < grade < 10:
+                lecturer.lecturer_grades[course] += [grade]
+            else:
+                lecturer.lecturer_grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
         
 class Mentor:
     def __init__(self, name, surname):
@@ -26,6 +36,7 @@ class Reviewer(Mentor):
     pass
 
 class Lecturer(Mentor):
+    lecturer_grades = {}
     def rate_hw(self, student, course, grade):
         print(f"Error: lecturer cannot review homework")
 
